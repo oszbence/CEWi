617a8cedc9c3f21d136d1755915bd616bef8c3a1
Olasz-Szabo Bence <oszb92@gmail.com>
2016-04-08 16:03:30 +0200

    Dagger 2 added

--- app/src/main/java/hu/cewi/client/user/MainActivity.java
+++ app/src/main/java/hu/cewi/client/user/MainActivity.java
@@ -1,10 +1,15 @@
 package hu.cewi.client.user;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
+import dagger.Module;
+
+@Module
 public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
